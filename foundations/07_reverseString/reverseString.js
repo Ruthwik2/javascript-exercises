a1d3b3de843596328bb10255b816f12afe35213f
Ruthwik2 <ruthwikveldandi@gmail.com>
2026-06-22 15:57:14 +0530

    exercise -7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+// strings are immutable so doing this will not change string
+const reverseString = function(string) {
+    return string.split('');
+            string.reverse();
+            string.join("");
 };
 
 // Do not edit below this line
