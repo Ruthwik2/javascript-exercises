99bb997f67a7c56822c7a31d4ae1c8233e2d65f7
Ruthwik2 <ruthwikveldandi@gmail.com>
2026-06-22 17:25:21 +0530

    exercise -11 toFixed returns string should be converted to number.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temperatureInFahrenheit) {
+  return Number((((temperatureInFahrenheit-32)*5)/9).toFixed(1));
+};  
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  return Number((((temperatureInCelsius*9)/5)+32).toFixed(1));
 };
 
 // Do not edit below this line
