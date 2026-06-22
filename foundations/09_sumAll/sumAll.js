22e39bc2bcd18fbad09fd692c6c2d069e1ab5542
Ruthwik2 <ruthwikveldandi@gmail.com>
2026-06-22 17:00:46 +0530

    exerxise -9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(
+        typeof(a)!=="number"||
+        typeof(b)!=="number"||
+        a<0||b<0||
+        !Number.isInteger(a)||
+        !Number.isInteger(b)
+    ){
+        return "ERROR";
+    }
+    if(b<a)[a,b]=[b,a];
+    return (b*(b+1)-(a*(a-1)))/2;
 };
 
 // Do not edit below this line
